a3014bcc3f665b16e1acb2307acc0c33909f79b7
BharathirajSundaram <sbharathiraj12@gmail.com>
2023-11-10 18:21:45 +0530

    b. Convert all the strings to title caps in a string array

--- DayFourTask/DayFourTask.js
+++ DayFourTask/DayFourTask.js
@@ -4,4 +4,20 @@ let arr = [];
 for (let i = 1; i < 100; i += 2) {
     arr.push(i);
 }
-console.log(arr);
+//console.log(arr);
+
+
+//b. Convert all the strings to title caps in a string array
+
+
+let str = "my name is bharathiraj sundaram";
+
+
+
+let output = str.split(' ')
+   .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase()).join(" ");
+//console.log(output);
+
+
+
+
