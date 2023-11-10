3531225ce9125187e8f9655f25ebc44f0bf23a47
BharathirajSundaram <sbharathiraj12@gmail.com>
2023-11-11 00:32:52 +0530

    d. Return all the prime numbers in an array

--- DayFourTask/DayFourTask.js
+++ DayFourTask/DayFourTask.js
@@ -6,32 +6,36 @@ for (let i = 1; i < 100; i += 2) {
 }
 //console.log(arr);
 
+//=========================================================================================
 
 //b. Convert all the strings to title caps in a string array
-
-
 let str = "my name is bharathiraj sundaram";
 
-
-
 let output = str.split(' ')
-   .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase()).join(" ");
+    .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase()).join(" ");
 //console.log(output);
 
+//=========================================================================================
 
 //c. Sum of all numbers in an array
+let inputArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let addition = inputArr.reduce((sum, value) => {
+    return sum += value;
+}, 0);
+//console.log(addition);
 
 
-let inputArr = [1,2,3,4,5,6,7,8,9,10]
+//=========================================================================================
 
-let addition=inputArr.reduce((sum,value)=>{
-    return sum +=value;
-    
-},0);
-console.log(addition);
-
-
-
-
+//d. Return all the prime numbers in an array
+let inputArr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+console.log(inputArr1);
+let primeNum = inputArr1.filter((value) => {
+    for (let i = 2; i <= Math.sqrt(value); i++) {
+        if (value % i === 0) { return false }
+    }
+    return true
+});
+console.log(primeNum);
 
 
