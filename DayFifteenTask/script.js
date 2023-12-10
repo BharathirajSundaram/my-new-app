1deae9283d8e95c8b88d7942ff67d09005b33b9e
BharathirajSundaram <sbharathiraj12@gmail.com>
2023-12-10 23:58:24 +0530

    display data and new styles

--- DayFifteenTask/script.js
+++ DayFifteenTask/script.js
@@ -1,116 +1,659 @@
-// selecting required element
-const element = document.querySelector(".pagination ul");
-let totalPages = 30;
-let page = 10;
+let tabledata = [
+  {
+    id: "1",
+    name: "Lura Senger",
+    email: "Xander_Collier@yahoo.com",
+  },
+  {
+    id: "2",
+    name: "Wilburn Weber",
+    email: "Bennett_Kreiger11@yahoo.com",
+  },
+  {
+    id: "3",
+    name: "Tyrique Hahn",
+    email: "Juston.Altenwerth@yahoo.com",
+  },
+  {
+    id: "4",
+    name: "Lenny Bailey",
+    email: "Guiseppe_Hegmann@yahoo.com",
+  },
+  {
+    id: "5",
+    name: "Vladimir Keeling",
+    email: "Louisa_Walsh18@hotmail.com",
+  },
+  {
+    id: "6",
+    name: "Kellie Crona",
+    email: "Chandler_Abernathy@yahoo.com",
+  },
+  {
+    id: "7",
+    name: "Carolina White",
+    email: "Royal50@hotmail.com",
+  },
+  {
+    id: "8",
+    name: "Alfredo Conn",
+    email: "Clarabelle34@hotmail.com",
+  },
+  {
+    id: "9",
+    name: "Stan Shanahan",
+    email: "Lamar.McClure@hotmail.com",
+  },
+  {
+    id: "10",
+    name: "Marvin Fay",
+    email: "Osbaldo58@hotmail.com",
+  },
+  {
+    id: "11",
+    name: "Torrance Rau",
+    email: "Orin45@gmail.com",
+  },
+  {
+    id: "12",
+    name: "Harold Gutmann MD",
+    email: "Alyce.Stracke37@yahoo.com",
+  },
+  {
+    id: "13",
+    name: "Taryn Torphy",
+    email: "Dean_Breitenberg71@hotmail.com",
+  },
+  {
+    id: "14",
+    name: "Bryana Lang",
+    email: "Tatum.Ullrich@yahoo.com",
+  },
+  {
+    id: "15",
+    name: "Nyasia Green DDS",
+    email: "Dino83@gmail.com",
+  },
+  {
+    id: "16",
+    name: "Nasir Gerhold",
+    email: "Lilian_Bashirian8@hotmail.com",
+  },
+  {
+    id: "17",
+    name: "Raymundo Ritchie PhD",
+    email: "Antwan.Schoen15@yahoo.com",
+  },
+  {
+    id: "18",
+    name: "Delmer Marvin",
+    email: "Kiera62@yahoo.com",
+  },
+  {
+    id: "19",
+    name: "Rachel Wilkinson",
+    email: "Foster_Conroy@gmail.com",
+  },
+  {
+    id: "20",
+    name: "Gladys Howell",
+    email: "Constance.Labadie10@yahoo.com",
+  },
+  {
+    id: "21",
+    name: "Ciara Klocko",
+    email: "Harvey76@yahoo.com",
+  },
+  {
+    id: "22",
+    name: "Quentin O'Kon",
+    email: "Amely_Cummings2@yahoo.com",
+  },
+  {
+    id: "23",
+    name: "Ms. Gabriella Kunde",
+    email: "Lorenza_Cummerata@hotmail.com",
+  },
+  {
+    id: "24",
+    name: "Gerald Reilly",
+    email: "Lia_Konopelski@gmail.com",
+  },
+  {
+    id: "25",
+    name: "Brody Carter I",
+    email: "Colten.Wilderman90@hotmail.com",
+  },
+  {
+    id: "26",
+    name: "Alanis Klocko",
+    email: "Johathan.Champlin69@gmail.com",
+  },
+  {
+    id: "27",
+    name: "Caroline Miller",
+    email: "Delaney.Kozey74@gmail.com",
+  },
+  {
+    id: "28",
+    name: "Ms. Merritt McClure",
+    email: "Wendy.Zieme@gmail.com",
+  },
+  {
+    id: "29",
+    name: "Jovani Schoen",
+    email: "Norval_Zieme@hotmail.com",
+  },
+  {
+    id: "30",
+    name: "Berniece Bradtke",
+    email: "Okey.Fisher39@hotmail.com",
+  },
+  {
+    id: "31",
+    name: "Hanna Gottlieb",
+    email: "Edna62@hotmail.com",
+  },
+  {
+    id: "32",
+    name: "Christiana Bergnaum",
+    email: "Donnie.Paucek22@gmail.com",
+  },
+  {
+    id: "33",
+    name: "Reid Stehr",
+    email: "Hiram69@gmail.com",
+  },
+  {
+    id: "34",
+    name: "Evans Ward",
+    email: "Liliane72@hotmail.com",
+  },
+  {
+    id: "35",
+    name: "Miss Enos Davis",
+    email: "Kolby.Grady@hotmail.com",
+  },
+  {
+    id: "36",
+    name: "Mathew Lueilwitz",
+    email: "Mariah.Hagenes@yahoo.com",
+  },
+  {
+    id: "37",
+    name: "Romaine Robel",
+    email: "Sheila19@yahoo.com",
+  },
+  {
+    id: "38",
+    name: "Immanuel Aufderhar",
+    email: "Aileen32@hotmail.com",
+  },
+  {
+    id: "39",
+    name: "Julius Reilly Jr.",
+    email: "Santa89@yahoo.com",
+  },
+  {
+    id: "40",
+    name: "Linnea Borer",
+    email: "Jarrett_Hegmann2@hotmail.com",
+  },
+  {
+    id: "41",
+    name: "Misael Abernathy IV",
+    email: "Robbie42@yahoo.com",
+  },
+  {
+    id: "42",
+    name: "Mr. Gerda Jerde",
+    email: "Gustave36@hotmail.com",
+  },
+  {
+    id: "43",
+    name: "Easton Daugherty",
+    email: "Malcolm.Schuster11@hotmail.com",
+  },
+  {
+    id: "44",
+    name: "Leanna Gutkowski",
+    email: "Chelsea9@gmail.com",
+  },
+  {
+    id: "45",
+    name: "Rollin Trantow",
+    email: "Marques_Hickle26@gmail.com",
+  },
+  {
+    id: "46",
+    name: "Heaven Luettgen",
+    email: "Lonnie.Greenfelder95@hotmail.com",
+  },
+  {
+    id: "47",
+    name: "Gilda Huel",
+    email: "Stephania.Hintz@gmail.com",
+  },
+  {
+    id: "48",
+    name: "Javon Walker",
+    email: "Lamar_Bailey39@yahoo.com",
+  },
+  {
+    id: "49",
+    name: "Stewart King",
+    email: "Chasity_Jenkins82@gmail.com",
+  },
+  {
+    id: "50",
+    name: "Cortez Lueilwitz",
+    email: "Lelia.Mitchell43@gmail.com",
+  },
+  {
+    id: "51",
+    name: "Ora Kilback",
+    email: "Matt25@gmail.com",
+  },
+  {
+    id: "52",
+    name: "Lera Macejkovic",
+    email: "Jamal.Donnelly45@hotmail.com",
+  },
+  {
+    id: "53",
+    name: "Asa Gottlieb",
+    email: "Carolyne.Langosh@yahoo.com",
+  },
+  {
+    id: "54",
+    name: "Avery Nienow",
+    email: "Hudson_Bernhard47@yahoo.com",
+  },
+  {
+    id: "55",
+    name: "Dr. Katrine Ryan",
+    email: "Colten40@gmail.com",
+  },
+  {
+    id: "56",
+    name: "Florida Kulas",
+    email: "Alejandra_Bernhard@yahoo.com",
+  },
+  {
+    id: "57",
+    name: "Mauricio Gerlach",
+    email: "Vincenza_Gusikowski84@hotmail.com",
+  },
+  {
+    id: "58",
+    name: "Chadrick Simonis",
+    email: "Reymundo.Doyle@gmail.com",
+  },
+  {
+    id: "59",
+    name: "Beau Jenkins",
+    email: "Cletus65@hotmail.com",
+  },
+  {
+    id: "60",
+    name: "Camilla Cole",
+    email: "Eda48@yahoo.com",
+  },
+  {
+    id: "61",
+    name: "Abagail MacGyver",
+    email: "Jevon.Koch@hotmail.com",
+  },
+  {
+    id: "62",
+    name: "Ludwig Prohaska",
+    email: "Eldon_DuBuque37@yahoo.com",
+  },
+  {
+    id: "63",
+    name: "Berenice Hane",
+    email: "Lonnie3@gmail.com",
+  },
+  {
+    id: "64",
+    name: "Abel Durgan",
+    email: "Marlee.Weimann42@gmail.com",
+  },
+  {
+    id: "65",
+    name: "Tate Kuhlman",
+    email: "Hope31@yahoo.com",
+  },
+  {
+    id: "66",
+    name: "Mrs. Hermina Daniel",
+    email: "Murphy59@yahoo.com",
+  },
+  {
+    id: "67",
+    name: "Marcelino Nienow",
+    email: "Estefania_Dibbert97@hotmail.com",
+  },
+  {
+    id: "68",
+    name: "Raphaelle Connelly",
+    email: "Josie.Legros70@yahoo.com",
+  },
+  {
+    id: "69",
+    name: "Mina Heaney",
+    email: "Pete.Hermann79@hotmail.com",
+  },
+  {
+    id: "70",
+    name: "Jaida Torp",
+    email: "Jonatan82@yahoo.com",
+  },
+  {
+    id: "71",
+    name: "Megane Fadel",
+    email: "Dandre82@yahoo.com",
+  },
+  {
+    id: "72",
+    name: "Angelo Schimmel V",
+    email: "Frederic54@hotmail.com",
+  },
+  {
+    id: "73",
+    name: "Jaren Ferry DDS",
+    email: "Kristian.Kilback@yahoo.com",
+  },
+  {
+    id: "74",
+    name: "Annette O'Connell",
+    email: "Domenica.Christiansen@gmail.com",
+  },
+  {
+    id: "75",
+    name: "Elenor Rohan PhD",
+    email: "Alvera.Huel48@yahoo.com",
+  },
+  {
+    id: "76",
+    name: "Linnie Beier",
+    email: "Reece_Berge@gmail.com",
+  },
+  {
+    id: "77",
+    name: "Maybelle Lehner",
+    email: "Arlo.Hagenes@gmail.com",
+  },
+  {
+    id: "78",
+    name: "Hailie Haag",
+    email: "Edythe.Lind59@hotmail.com",
+  },
+  {
+    id: "79",
+    name: "Marcelina Schuppe",
+    email: "Kip.Cummerata77@gmail.com",
+  },
+  {
+    id: "80",
+    name: "Ezra Lang",
+    email: "Garret.Swaniawski@hotmail.com",
+  },
+  {
+    id: "81",
+    name: "Carey Kiehn",
+    email: "Sabrina70@gmail.com",
+  },
+  {
+    id: "82",
+    name: "Carole Aufderhar",
+    email: "Efrain.Schamberger77@hotmail.com",
+  },
+  {
+    id: "83",
+    name: "Fanny Beatty",
+    email: "Ewald28@yahoo.com",
+  },
+  {
+    id: "84",
+    name: "Mina Lubowitz",
+    email: "Vincenzo.Herman@gmail.com",
+  },
+  {
+    id: "85",
+    name: "Anabelle Thiel",
+    email: "Amely.Renner@hotmail.com",
+  },
+  {
+    id: "86",
+    name: "Gerry Homenick",
+    email: "Ada_Von54@hotmail.com",
+  },
+  {
+    id: "87",
+    name: "Domenico Maggio",
+    email: "Freeman.Stamm@yahoo.com",
+  },
+  {
+    id: "88",
+    name: "Mauricio Bradtke III",
+    email: "Alford83@yahoo.com",
+  },
+  {
+    id: "89",
+    name: "Bulah Yost III",
+    email: "Nicolas_Schimmel6@yahoo.com",
+  },
+  {
+    id: "90",
+    name: "Orville Dibbert DDS",
+    email: "Bryana_Herzog@yahoo.com",
+  },
+  {
+    id: "91",
+    name: "Autumn Osinski",
+    email: "Davin8@hotmail.com",
+  },
+  {
+    id: "92",
+    name: "Levi Gibson",
+    email: "Deangelo69@gmail.com",
+  },
+  {
+    id: "93",
+    name: "Reva McKenzie",
+    email: "Bret40@hotmail.com",
+  },
+  {
+    id: "94",
+    name: "Jensen Pfannerstill",
+    email: "Dax_Marquardt@hotmail.com",
+  },
+  {
+    id: "95",
+    name: "Tyrese Larson MD",
+    email: "Cecelia85@yahoo.com",
+  },
+  {
+    id: "96",
+    name: "Dayne Sanford",
+    email: "Tyrique_Waters16@hotmail.com",
+  },
+  {
+    id: "97",
+    name: "Jeramy Steuber",
+    email: "Catalina.Stehr24@yahoo.com",
+  },
+  {
+    id: "98",
+    name: "Madge Bogisich",
+    email: "Joey.Marquardt50@gmail.com",
+  },
+  {
+    id: "99",
+    name: "Casimer MacGyver",
+    email: "Julius.Bednar96@yahoo.com",
+  },
+  {
+    id: "100",
+    name: "Arlene Kassulke",
+    email: "Josiane_Smith@yahoo.com",
+  },
+];
 
-//calling function with passing parameters and adding inside element which is ul tag
-element.innerHTML = createPagination(totalPages, page);
-function createPagination(totalPages, page){
-  let liTag = '';
-  let active;
-  let beforePage = page - 1;
-  let afterPage = page + 1;
-  if(page > 1){ //show the next button if the page value is greater than 1
-    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
-  }
+let body = document.querySelector(`body`);
 
-  if(page > 2){ //if page value is less than 2 then add 1 after the previous button
-    liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`;
-    if(page > 3){ //if page value is greater than 3 then add this (...) after the first li or page
-      liTag += `<li class="dots"><span>...</span></li>`;
-    }
-  }
+let title = document.createElement(`h1`);
+title.setAttribute("id", "title");
+title.innerText = "DOM Pagination Task ";
 
-  // how many pages or li show before the current li
-  if (page == totalPages) {
-    beforePage = beforePage - 2;
-  } else if (page == totalPages - 1) {
-    beforePage = beforePage - 1;
-  }
-  // how many pages or li show after the current li
-  if (page == 1) {
-    afterPage = afterPage + 2;
-  } else if (page == 2) {
-    afterPage  = afterPage + 1;
-  }
+let p = document.createElement("p");
+p.setAttribute("id", "description");
+body.append(p);
+p.append(title);
 
-  for (var plength = beforePage; plength <= afterPage; plength++) {
-    if (plength > totalPages) { //if plength is greater than totalPage length then continue
-      continue;
-    }
-    if (plength == 0) { //if plength is 0 than add +1 in plength value
-      plength = plength + 1;
-    }
-    if(page == plength){ //if page is equal to plength than assign active string in the active variable
-      active = "active";
-    }else{ //else leave empty to the active variable
-      active = "";
-    }
-    liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
-  }
+let div = document.createElement("div");
+div.setAttribute("id", "div-table");
+div.classList.add("table-responsive"); //ClassList (div new class)
+body.append(div);
 
-  if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
-    if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
-      liTag += `<li class="dots"><span>...</span></li>`;
-    }
-    liTag += `<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
-  }
+let table1 = document.createElement("table");
+table1.classList.add("table", "table-bordered");
+table1.setAttribute("id", "table");
+div.append(table1);
 
-  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
-    liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
-  }
-  element.innerHTML = liTag; //add li tag inside ul tag
-  return liTag; //reurn the li tag
+let thead = document.createElement("thead");
+thead.setAttribute("id", "table-head");
+thead.classList.add("table-bordered");
+table1.append(thead);
 
-  
+let tr = document.createElement("tr");
+thead.append(tr);
 
+let th1 = document.createElement("th");
+th1.innerText = "Id";
+let th2 = document.createElement("th");
+th2.innerText = "Name";
+let th3 = document.createElement("th");
+th3.innerText = "Email";
+
+tr.append(th1);
+tr.append(th2);
+tr.append(th3);
+
+let div1 = document.createElement("div");
+div1.classList.add("d-flex", "justify-content-center");
+div1.setAttribute("id", "buttons");
+let div2 = document.createElement("div");
+div2.setAttribute("id", "pagination-wrapper");
+div2.setAttribute("class", "outer-shadow hover-in-shadow");
+div1.append(div2);
+body.append(div1);
+
+var state = {
+  querySet: tabledata,
+  page: 1,
+  rows: 10,
+  window: 5,
+};
+
+buildTable();
+
+function pagination(querySet, pages, rows) {
+  var trimStart = (pages - 1) * rows;
+  var trimEnd = trimStart + rows;
+
+  var trimmedData = querySet.slice(trimStart, trimEnd);
+  var pages = Math.ceil(querySet.length / rows);
+
+  return {
+    querySet: trimmedData,
+    pages: pages,
+  };
 }
 
+function pageButtons(pages) {
+  var wrapper = document.getElementById("pagination-wrapper");
+  wrapper.innerHTML = ``;
+  console.log("Pages:", pages);
+  console.log("window", state.window);
 
+  var maxLeft = Number(state.page) - Math.floor(state.window / 2);
+  var maxRight = Number(state.page) + Math.floor(state.window / 2);
 
-var pageLists = fetch('./pagination.json').then((response) => response.json())
-    // .then((json) => {console.log(json[0])})
-    // .then((json) => document.createElement('divId').innerText = json[0].name);
+  console.log("maxLeft f", maxLeft);
+  console.log("maxRigth f", maxRight);
 
-let http = new XMLHttpRequest();    
+  if (maxLeft < 1) {
+    maxLeft = 1;
+    maxRight = state.window;
+  }
 
-http.open('GET','./pagination.json');
-http.send();
-http.onload= () => {
-  if(http.status==200){
-    let resBody= http.response;
+  if (maxRight > pages) {
+    maxRight = pages;
+    maxLeft = pages - (state.window - 1);
 
-
-
-
-    let resType = typeof resBody;
-    let jsonObj= JSON.parse(resBody);
-    console.log(typeof jsonObj)
-    console.log(resBody)
-    console.log(resType)
-    
-
-
-    
-let dataList=document.createElement("div");
-dataList.setAttribute('id','data-table');
-
-dataList.innerText='<table><tr><th>'+item.id+'</th></tr></table>';
-    
-for (const item of jsonObj) {
-      console.log(item.id,">>>>>>>>>>>>",item.name,">>>>>>>>>>>>",item.email )
-      
+    if (maxLeft < 1) {
+      maxLeft = 1;
     }
+  }
+  console.log("maxLeft", maxLeft);
+  console.log("maxRigth", maxRight);
 
+  for (var page = maxLeft; page <= maxRight; page++) {
+    wrapper.innerHTML =
+      wrapper.innerHTML +
+      `<button id=${page} onclick="btn(this)" class="page btn btn-sm btn-info">${page}</button>`;
+  }
+
+  if (state.page != 1) {
+    wrapper.innerHTML =
+      `<button id=${1} onclick="btn(this)" class="page btn btn-sm btn-info"> << </button>` +
+      `<button id=${
+        state.page - 1
+      } onclick="btn(this)" class="page btn btn-sm btn-info"><</button>` +
+      wrapper.innerHTML;
+  }
+
+  if (state.page != pages) {
+    wrapper.innerHTML =
+      wrapper.innerHTML +
+      `<button id=${
+        Number(state.page) + 1
+      } onclick="btn(this)" class="page btn btn-sm btn-info">></button>` +
+      `<button id=${pages} onclick="btn(this)" class="page btn btn-sm btn-info"> >></button>`;
   }
 }
 
+function btn(e) {
+  let k = document.getElementById("table-body");
+  let parent = k.parentNode;
+  parent.removeChild(k);
+  console.log("e", e.id);
+  state.page = e.id;
+  console.log("stage page", state.page);
+  buildTable();
+}
 
+var data;
+function buildTable() {
+  data = pagination(state.querySet, state.page, state.rows);
+  console.log("Data:", data);
 
-console.log(pageLists);
-  for(let index=0;index<pageLists.length;index++){
-    html+='<li>response.json</li>';
-   }
+  var myList = data.querySet;
 
-  let el=  document.createElement('list');
-  el.appendChild ='<li>${pageList[index]}</li>';
+  let tbody = document.createElement("tbody");
+  tbody.setAttribute("id", "table-body");
+  table1.append(tbody);
+
+  for (let i = 0; i < myList.length; i++) {
+    tr = document.createElement("tr");
+    for (let j of Object.keys(myList[i])) {
+      let td = document.createElement("td");
+      td.innerText = myList[i][j];
+      tr.append(td);
+    }
+    tbody.append(tr);
+  }
+  pageButtons(data.pages);
+}
